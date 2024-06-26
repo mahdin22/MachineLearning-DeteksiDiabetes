6956bcb9b97713f0342ae787acd619557c4246af
mahdin22 <128858135+mahdin22@users.noreply.github.com>
2024-06-26 07:47:30 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from Streamlit import streamlit, render_template, request, redirect
+from flask import flask, render_template, request, redirect
 import pickle
 import sklearn
 import numpy as np                        # numpy==1.19.3
 
-app = Streamlit(__name__)
+app = flask(__name__)
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
